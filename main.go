fd55ea63fff67d90631c6218b116185b615bc962
Seifbarouni <seif.barouni11@gmail.com>
2020-08-16 20:02:20 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+var str string
+
+func palindrome(str string)bool{
+	newStr:=trim(str)
+	fmt.Println(newStr)
+	length:=len(newStr)
+
+	 bound:=(length/2)+1
+	var i int
+	for i =0; i < bound; i++ {
+		if newStr[i]!=newStr[length-1-i]{
+			return false
+		}
+	}
+	return true
+}
+func trim(s string)string{
+	res:=strings.Trim(s,"!@#$%^&*()*?><.,;''/| ")
+	
+	
+	return strings.ToLower(strings.ReplaceAll(res," ",""))
+}
+
+
+func main() {
+	r:=gin.Default()
+	r.LoadHTMLGlob("template/*.html")
+
+	r.GET("/",func(c *gin.Context){
+		c.Redirect(http.StatusFound,"/Pal")
+	})
+	r.GET("/Pal",func(c *gin.Context ){
+		c.HTML(200,"index.html",str)
+	})
+	r.POST("/Pal",func(c *gin.Context){
+		strToCheck:=c.PostForm("txt")
+		valid:=palindrome(strToCheck)
+
+		if valid == true{
+			str=fmt.Sprintf("'%s' is a Palindrome",strToCheck)
+		}else{
+			str=fmt.Sprintf("'%s' is not a Palindrome",strToCheck)
+		}
+		c.Redirect(http.StatusFound,"/Pal")
+
+	})
+
+	r.Run()
+}
